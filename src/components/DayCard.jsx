f3a419019a40f0608ffd1490309e0ba3ab58a4cc
Vadim Kogol <vadim.kogol@irlix.ru>
2024-01-04 03:09:06 +0400

    change

--- src/components/DayCard.jsx
+++ src/components/DayCard.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
+import { days } from '../constants'
 
-export default ({ nameDay = 'nameDay', temp = 'temp' }) => {
+export default ({ info }) => {
+  const nameDay = days[new Date(info.dt_txt).getDay()]
+  const weather = info.weather[0].description
+
   return null
 }
